c1d44acf1aa4753e5c59eefaa73506f2188850d6
a01074882716 <a01074882716@gmail.com>
2023-03-08 13:41:29 +0900

    search 컨트롤러 및 주소 th 완료

---
+++ HelloPet/src/main/java/kr/co/hellopet/controller/search/SearchController.java
@@ -0,0 +1,39 @@
+package kr.co.hellopet.controller.search;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SearchController {
+	
+	@GetMapping("search/index")
+	public String index() {
+		return "search/index";
+	}
+	
+	@GetMapping("search/reserve")
+	public String reserve() {
+		return "search/reserve";
+	}
+	
+	@GetMapping("search/complete")
+	public String complete() {
+		return "search/complete";
+	}
+	
+	@GetMapping("search/view")
+	public String view() {
+		return "search/view";
+	}
+	
+	@GetMapping("search/SearchHs")
+	public String SearchHs() {
+		return "search/SearchHs";
+	}
+	
+	@GetMapping("search/SearchPm")
+	public String SearchPm() {
+		return "search/SearchPm";
+	}
+	
+}
