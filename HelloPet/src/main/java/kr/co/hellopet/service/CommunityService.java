4add532b8ef4364c827b216fc743b6ae60996724
sorkcoavus <sorkcoavus@gmail.com>
2023-03-09 13:42:45 +0900

    3/9 화면구현완료, tip 기능구현 글쓰기(파일제외)완료

--- HelloPet/src/main/java/kr/co/hellopet/service/CommunityService.java
+++ HelloPet/src/main/java/kr/co/hellopet/service/CommunityService.java
@@ -1,8 +1,25 @@
 package kr.co.hellopet.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import kr.co.hellopet.dao.CommunityDAO;
+import kr.co.hellopet.vo.CommunityVO;
+
+/*
+ * 날짜 : 2023/03/09
+ * 이름 : 김경준
+ * 설명 : HelloPet 커뮤니티 페이지 기능구현
+ */
 
 @Service
 public class CommunityService {
 
+	@Autowired
+	private CommunityDAO dao;
+	
+	// tip 글쓰기
+	public int insertTipArticle(CommunityVO vo) {
+		return dao.insertTipArticle(vo);
+	}
 }
