a4084e8d5840b42db816c8c23fbe4e19dcb0d47a
imminji000 <swlss79@naver.com>
2023-03-09 13:42:12 +0900

    Feat : desease

---
+++ HelloPet/src/main/java/kr/co/hellopet/service/DiseaseService.java
@@ -0,0 +1,13 @@
+package kr.co.hellopet.service;
+
+import org.springframework.stereotype.Service;
+
+/*
+ * 날짜 : 2023/03/09
+ * 이름 : 임민지
+ * 내용 : Disease Service 기능구현 
+ */
+@Service
+public class DiseaseService {
+
+}
