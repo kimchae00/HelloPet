ae1e99dcd03a297c58659bde7879a6c67518687f
leemh44390 <leemh44390@gmail.com>
2023-03-10 13:30:29 +0900

    2023/03/10 register Medical

---
+++ HelloPet/src/main/java/kr/co/hellopet/vo/Api_PharmacyVO.java
@@ -0,0 +1,17 @@
+package kr.co.hellopet.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Api_PharmacyVO {
+	
+	private int pharNo;
+	private String pharName;
+	private String pharAddr;
+	private double x;
+	private double y;
+	private String tel;
+
+}
